64a1cd324c144a73ad51d9e9d709d12384732690
OhrimenkoIgor <ihor.okhrimenko@globallogic.com>
2013-07-17 11:14:05 +0300

    chapter 8 tasks added

--- chapter8/task6/task6.cpp
+++ chapter8/task6/task6.cpp
@@ -1,19 +1,79 @@
-/*
-Write a template function maxn() that takes as its arguments an array of items of
-typeTand an integer representing the number of elements in the array and that
-returns the largest item in the array.Test it in a program that uses the function template 
-with an array of six intvalue and an array of four doublevalues.The program 
-should also include a specialization that takes an array of pointers-to-charas
-an argument and the number of pointers as a second argument and that returns the
-address of the longest string.If multiple strings are tied for having the longest
-length,the function should return the address of the first one tied for longest.Test
-the specialization with an array of five string pointers.
-*/
 
 #include <iostream>
+#include <cstring>
+
+using namespace std;
+
+template <typename T>
+T maxn(T arr[], int n){
+	T max = arr[0];
+	for(int i = 1; i < n; i++){
+		if (arr[i] > max){
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+template <> char * maxn(char * arr[], int n){
+	char * max = arr[0];
+	unsigned int max_len = strlen(arr[0]);
+	unsigned int len;
+	for(int i = 1; i < n; i++){
+		len = strlen(arr[i]);
+		if (len > max_len){
+			max = arr[i];
+			max_len = len;
+		}
+	}
+	return max;
+}
+
+template <typename T> void printn(T arr[], int n);
+template <> void printn(char * arr[], int n);
 
 int main (){
 
+	int  iarr[6] = {1, 7, 2, 6, 2, 5};
+	double darr[4] = {5.1, 6.7, 4.2, 6.5};
+	char * carr[5] = {
+		"First string",
+		"Second string",
+		"Third string",
+		"Fourth string",
+		"Fifth string"
+	};
+
+
+	cout << "Int arr: " << endl;
+	printn(iarr, 6);
+	cout << "Max int: " << maxn(iarr, 6) << endl;
+	cout << endl;
+
+	cout << "Double arr: " << endl;
+	printn(darr, 4);
+	cout << "Max double: " << maxn(darr, 4) << endl;
+	cout << endl;
+
+	cout << "char * arr: " << endl;
+	printn(carr, 5);
+	cout << "Max string: " << maxn(carr, 5) << endl;
+	cout << endl;
+
+	cin.get();
 
 	return 0;
+}
+
+template <typename T> void printn(T arr[], int n){
+	for(int i = 0; i < n; i++){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+template <> void printn(char * arr[], int n){
+	for(int i = 0; i < n; i++){
+		cout << arr[i] << endl;
+	}
 }
