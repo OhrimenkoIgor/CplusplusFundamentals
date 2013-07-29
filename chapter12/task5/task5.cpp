2a02c8b93e3d782dab5f76ad8b0a6247ba06a49e
OhrimenkoIgor <ihor.okhrimenko@globallogic.com>
2013-07-29 12:33:15 +0300

    chapter 12 tasks added

--- chapter12/task5/task5.cpp
+++ chapter12/task5/task5.cpp
@@ -1,7 +1,92 @@
 
-/*
-The Bank of Heather has performed a study showing that ATM customers won’t
-wait more than one minute in line.Using the simulation from Listing 12.10,find a
-value for number of customers per hour that leads to an average wait time of one
-minute.(Use at least a 100-hour trial period.)
-*/
+// bank.cpp -- using the Queue interface
+// compile with queue.cpp
+#include <iostream>
+#include <cstdlib> // for rand() and srand()
+#include <ctime> // for time()
+#include "queue.h"
+const int MIN_PER_HR = 60;
+const int SIMULATION_HOURS = 100;
+const double TIME_LIMIT = 1.0;
+const double TIME_STEP = 0.05;
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ios_base;
+
+bool newcustomer(double x); // is there a new customer?
+
+double simulate_wait_time(int queue_size, int simulation_hours, double customers_per_hour);
+
+int main()
+{
+	// setting things up
+	std::srand(std::time(0)); // random initializing of rand()
+	cout << "Case Study: Bank of Heather Automatic Teller\n";
+	cout << "Enter maximum size of queue: ";
+	int qs;
+	(cin >> qs).get();
+	
+	double customers_per_hour = 0;
+	for (double i = 0; ; i +=TIME_STEP){
+		if(simulate_wait_time(qs, SIMULATION_HOURS, i) > TIME_LIMIT){
+			break;
+		}
+		customers_per_hour = i;
+	}
+	cout << customers_per_hour << " customers per hour leads to an average wait time of " << TIME_LIMIT << " minute" << endl;
+
+	cin.get();
+	return 0;
+}
+// x = average time, in minutes, between customers
+// return value is true if customer shows up this minute
+bool newcustomer(double x)
+{
+	return (std::rand() * x / RAND_MAX < 1);
+}
+
+double simulate_wait_time(int qs, int hours, double perhour){
+	
+	Queue line(qs); // line queue holds up to qs people
+	// simulation will run 1 cycle per minute
+	long cyclelimit = MIN_PER_HR * hours; // # of cycles
+	double min_per_cust; // average time between arrivals
+	min_per_cust = MIN_PER_HR / perhour;
+	Item temp; // new customer data
+	long turnaways = 0; // turned away by full queue
+	long customers = 0; // joined the queue
+	long served = 0; // served during the simulation
+	long sum_line = 0; // cumulative line length
+	int wait_time = 0; // time until autoteller is free
+	long line_wait = 0; // cumulative time in line
+	// running the simulation
+	for (int cycle = 0; cycle < cyclelimit; cycle++)
+	{
+		if (newcustomer(min_per_cust)) // have newcomer
+		{
+			if (line.isfull())
+				turnaways++;
+			else
+			{
+				customers++;
+				temp.set(cycle); // cycle = time of arrival
+				line.enqueue(temp); // add newcomer to line
+			}
+		}
+		if (wait_time <= 0 && !line.isempty())
+		{
+			line.dequeue (temp); // attend next customer
+			wait_time = temp.ptime(); // for wait_time minutes
+			line_wait += cycle - temp.when();
+			served++;
+		}
+		if (wait_time > 0)
+			wait_time--;
+		sum_line += line.queuecount();
+	}
+
+	return (double) line_wait / served;
+
+}
--- chapter12/task6/task6.cpp
+++ chapter12/task6/task6.cpp
@@ -1,11 +1,107 @@
 
-/*
-The Bank of Heather would like to know what would happen if it added a second
-ATM.Modify the simulation in this chapter so that it has two queues. Assume that
-a customer will join the first queue if it has fewer people in it than the second
-queue and that the customer will join the second queue otherwise. Again,find a
-value for number of customers per hour that leads to an average wait time of one
-minute. (Note:This is a nonlinear problem in that doubling the number of ATMs
-doesn’t double the number of customers who canbe handled per hourwith a one-
-minute wait maximum.)
-*/
+// bank.cpp -- using the Queue interface
+// compile with queue.cpp
+#include <iostream>
+#include <cstdlib> // for rand() and srand()
+#include <ctime> // for time()
+#include "queue.h"
+const int MIN_PER_HR = 60;
+const int SIMULATION_HOURS = 100;
+const double TIME_LIMIT = 1.0;
+const double TIME_STEP = 0.05;
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ios_base;
+
+bool newcustomer(double x); // is there a new customer?
+
+double simulate_wait_time(int queue_size, int simulation_hours, double customers_per_hour);
+
+int main()
+{
+	// setting things up
+	std::srand(std::time(0)); // random initializing of rand()
+	cout << "Case Study: Bank of Heather Automatic Teller\n";
+	cout << "Enter maximum size of queue: ";
+	int qs;
+	(cin >> qs).get();
+	
+	double customers_per_hour = 0;
+	for (double i = 0; ; i +=TIME_STEP){
+		if(simulate_wait_time(qs, SIMULATION_HOURS, i) > TIME_LIMIT){
+			break;
+		}
+		customers_per_hour = i;
+	}
+	cout << customers_per_hour << " customers per hour leads to an average wait time of " << TIME_LIMIT << " minute" << endl;
+
+	cin.get();
+	return 0;
+}
+// x = average time, in minutes, between customers
+// return value is true if customer shows up this minute
+bool newcustomer(double x)
+{
+	return (std::rand() * x / RAND_MAX < 1);
+}
+
+double simulate_wait_time(int qs, int hours, double perhour){
+	
+	Queue line1(qs); // line queue holds up to qs people
+	Queue line2(qs); // line queue holds up to qs people
+	// simulation will run 1 cycle per minute
+	long cyclelimit = MIN_PER_HR * hours; // # of cycles
+	double min_per_cust; // average time between arrivals
+	min_per_cust = MIN_PER_HR / perhour;
+	Item temp; // new customer data
+	long turnaways = 0; // turned away by full queue
+	long customers = 0; // joined the queue
+	long served = 0; // served during the simulation
+	//long sum_line = 0; // cumulative line length
+	int wait_time1 = 0; // time until autoteller is free
+	int wait_time2 = 0; // time until autoteller is free
+	long line_wait = 0; // cumulative time in line
+	// running the simulation
+	for (int cycle = 0; cycle < cyclelimit; cycle++)
+	{
+		if (newcustomer(min_per_cust)) // have newcomer
+		{
+			if (line1.isfull() && line2.isfull())
+				turnaways++;
+			else
+			{
+				customers++;
+				temp.set(cycle); // cycle = time of arrival
+				if (line1.queuecount() <= line2.queuecount()){
+					line1.enqueue(temp); // add newcomer to line
+				} else{
+					line2.enqueue(temp);
+				}
+			}
+		}
+		if (wait_time1 <= 0 && !line1.isempty())
+		{
+			line1.dequeue (temp); // attend next customer
+			wait_time1 = temp.ptime(); // for wait_time minutes
+			line_wait += cycle - temp.when();
+			served++;
+		}
+		if (wait_time2 <= 0 && !line2.isempty())
+		{
+			line2.dequeue (temp); // attend next customer
+			wait_time2 = temp.ptime(); // for wait_time minutes
+			line_wait += cycle - temp.when();
+			served++;
+		}
+		if (wait_time1 > 0)
+			wait_time1--;
+		if (wait_time2 > 0)
+			wait_time2--;
+		//sum_line += line1.queuecount();
+	}
+
+	return (double) line_wait / served;
+
+}
